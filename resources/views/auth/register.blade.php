972ae30625c1d2977b256fabd42abc58cf8b10a4
Johann Hernandez <johannhernandez1233@gmail.com>
2024-05-25 23:50:29 +0800

    Nice Admin Successfully Integrated!

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,58 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
+    <div class="pt-4 pb-2">
+        <h5 class="card-title text-center pb-0 fs-4">Create an Account</h5>
+        <p class="text-center small">Enter your personal details to create account</p>
+    </div>
+        <form method="POST" action="{{ route('register') }}" class="row g-3">
+            @csrf
+    
+            <!-- Name -->
+            <div class="col-12">
+                <x-input-label for="name" :value="__('Name')" class="form-label" />
+                <x-text-input id="name" class="form-control" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            </div>
+    
+            <!-- Email Address -->
+            <div class="col-12">
+                <x-input-label for="email" :value="__('Email')" class="form-label"/>
+                <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+    
+            <!-- Password -->
+            <div class="col-12">
+                <x-input-label for="password" :value="__('Password')" class="form-label" />
+    
+                <x-text-input id="password" class="form-control"
+                                type="password"
+                                name="password"
+                                required autocomplete="new-password" />
+    
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+    
+            <!-- Confirm Password -->
+            <div class="col-12">
+                <x-input-label for="password_confirmation" :value="__('Confirm Password')" class="form-label" />
+    
+                <x-text-input id="password_confirmation" class="form-control"
+                                type="password"
+                                name="password_confirmation" required autocomplete="new-password" />
+    
+                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            </div>
+    
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+                <div class="col-12 mt-3">
+                    <x-primary-button class="btn btn-primary w-100">
+                        {{ __('Register') }}
+                    </x-primary-button>
+                </div>
+                
+            </div>
+        </form>
 </x-guest-layout>
--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -13,26 +13,32 @@
         @csrf
         @method('put')
 
-        <div>
-            <x-input-label for="update_password_current_password" :value="__('Current Password')" />
-            <x-text-input id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
+        <div class="row mb-3">
+            <x-input-label for="update_password_current_password" :value="__('Current Password')" class="col-form-label"/>
+            <div class="col-md-8 col-lg-4">
+                <x-text-input id="update_password_current_password" name="current_password" type="password" class="form-control" autocomplete="current-password" />
+            </div>
             <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
         </div>
 
-        <div>
-            <x-input-label for="update_password_password" :value="__('New Password')" />
-            <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+        <div class="row mb-3">
+            <x-input-label for="update_password_password" :value="__('New Password')" class="col-form-label"/>
+            <div class="col-md-8 col-lg-4">
+                <x-text-input id="update_password_password" name="password" type="password" class="form-control" autocomplete="new-password" />
+            </div>
             <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
         </div>
 
-        <div>
-            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+        <div class="row mb-3">
+            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" class="col-form-label"/>
+            <div class="col-md-8 col-lg-4">
+                <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="form-control" autocomplete="new-password" />
+            </div>
             <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <x-primary-button class="btn btn-primary">{{ __('Save') }}</x-primary-button>
 
             @if (session('status') === 'password-updated')
                 <p
